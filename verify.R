2b5fd1302896a15fc91c21108461b6e37d7975f1
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-10-14 09:39:46 +0000

    implement is_partial_room

--- verify.R
+++ verify.R
@@ -37,33 +37,16 @@ n_filled_cells <- function(R) {
     nrow()
 }
 
-is_room <- function(R) {
+is_partial_room <- function(R) {
   expected_number_of_distinct_pairs <- choose(max(R$col) + 1, 2)
   nfc <- n_filled_cells(R)
   is_row_latin(R) && 
     is_col_latin(R) && 
-    n_filled_cells(R) <= expected_number_of_distinct_pairs &&
-    nrow(distinct_pairs(R)) == nfc
+    n_filled_cells(R) <= expected_number_of_distinct_pairs
 }
 
-update <- function(R, i, j, first_, second_) {
-  R[R$row == i & R$col == j & R$name == "first", "value"] <- first_
-  R[R$row == i & R$col == j & R$name == "second", "value"] <- second_
-  return(R)
-}
-
-empty_cells <- function(R) {
-  R %>%
-    filter(is.na(value))
-}
-
-all_pairs <- function(n) {
-  
-  x <- combn(0:n, 2)
-  
-  tibble(
-    first = x[1,],
-    second = x[2,]
-  )
-  
+# does this make sense?
+is_room <- function(R) {
+  nfc <- n_filled_cells(R)
+  is_partial_room(R) && nrow(distinct_pairs(R)) == nfc
 }
