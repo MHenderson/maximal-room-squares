460a57ae902b1782ef663d5df7c7089b97b01c9d
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-10-14 15:43:15 +0000

    implement is_maximal_room

---
+++ R/is_maximal_room.R
@@ -0,0 +1,21 @@
+is_maximal_room <- function(R) {
+ 
+  result <- TRUE
+  
+  # iterate through the set of unusued pairs trying to place them
+  # return true if and only if no pairs can be placed
+  for(p in not_used_pairs(R)) {
+    
+    # try to find a hole
+    x <- first_available_cell(R, p)
+    
+    # if we were successful then fill that hole
+    if(!is.null(x))  {
+      result <- FALSE
+      break()
+    }
+    
+  }
+  
+  return(result)
+}
