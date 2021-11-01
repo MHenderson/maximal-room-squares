c911184daf8b3d11080d4135ac66c5e65bf87b1b
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-11-01 11:15:36 +0000

    implement a small optimisation for greedy3

--- R/greedy3.R
+++ R/greedy3.R
@@ -1,6 +1,7 @@
 greedy3 <- function(R, P = not_used_pairs(R), E = empty_cells(R)) {
   
   # log the available elements and pairs at each step
+  # init_basic_log
   X <- tribble(~row, ~col, ~available, ~Pe, ~first, ~second, ~fill, ~iter, ~branch)
   
   i <- 0
@@ -13,20 +14,29 @@ greedy3 <- function(R, P = not_used_pairs(R), E = empty_cells(R)) {
     # if there are fewer than 2 available symbols then move to the next empty cell
     # branch "fewer than 2 available symbols"
     if(length(available) < 2) {
+      # update_basic_log
       X <- X %>% log_to_df(e[1], e[2], list(available),  NA, NA, NA, FALSE, i, "less than 2 available symbols")
+      # update_full_log
       next()
     }
     
-    # a tiny optimisation is possible: if available is
-    # the whole set then we can just choose the first element of P
-    # because P is certain to be a subset of all pairs
-    # from available elements
-    Pe <- intersect_G(P, combn(available, 2, simplify = FALSE))
+    # if every symbol is available then the list of pairs available
+    # for this cell is the same as the list of globally avaiable pairs
+    # otherwise we only consider those pairs made from subsets of
+    # available symbols.
+    # this is a tiny optimisation and might not even be worth bothering with
+    if(length(available)==max(R$row) + 1) {
+      Pe <- P
+    } else {
+      Pe <- intersect_G(P, combn(available, 2, simplify = FALSE))
+    }
     
     # if there are none then move to the next empty cell
     # branch "only available pairs already used"
     if(length(Pe) < 1) {
+      # update_basic_log
       X <- X %>% log_to_df(e[1], e[2], list(available), NA, NA, NA, FALSE, i, "only available pairs already used")
+      # update_full_log
       next()
     }
     
