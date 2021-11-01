bb9cc141aec3de832b465f51a4bef607597fa538
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-11-01 10:24:09 +0000

    log iterations and branch

--- R/greedy3.R
+++ R/greedy3.R
@@ -3,6 +3,7 @@ greedy3 <- function(R, X) {
   P <- not_used_pairs(R)
   
   E <- empty_cells(R)
+  i <- 0
   
   for(e in E) {
     
@@ -10,8 +11,9 @@ greedy3 <- function(R, X) {
     available <- R[R$row == e[1] & R$col == e[2], "avail"]$avail[[1]]
     
     # if there are fewer than 2 available symbols then move to the next empty cell
+    # branch "fewer than 2 available symbols"
     if(length(available) < 2) {
-      X <- X %>% log_to_df(e[1], e[2], list(available),  NA, FALSE)
+      X <- X %>% log_to_df(e[1], e[2], list(available),  NA, NA, NA, FALSE, i, "less than 2 available symbols")
       next()
     }
     
@@ -24,12 +26,11 @@ greedy3 <- function(R, X) {
     Pe <- intersect_G(P, combn(available, 2, simplify = FALSE))
     
     # if there are none then move to the next empty cell
+    # branch "only available pairs already used"
     if(length(Pe) < 1) {
-      X <- X %>% log_to_df(e[1], e[2], list(available), NA, FALSE)
+      X <- X %>% log_to_df(e[1], e[2], list(available), NA, NA, NA, FALSE, i, "only available pairs already used")
       next()
     }
-    
-    X <- X %>% log_to_df(e[1], e[2],  available,  Pe,  TRUE)
     
     # X <- X %>%
     #   bind_rows(
@@ -39,6 +40,9 @@ greedy3 <- function(R, X) {
     
     # choose the first suitable pair
     p <- Pe[[1]]
+    
+    # branch "assign"
+    X <- X %>% log_to_df(e[1], e[2],  available,  Pe, p[1], p[2],  TRUE, i, "assign")
     
     # assign p to cell e,
     R[R$row == e[1] & R$col == e[2], "first"] <- p[1]
@@ -53,6 +57,8 @@ greedy3 <- function(R, X) {
     R[R$row == e[1], "avail"]$avail <- lapply(R[R$row == e[1], "avail"]$avail, remove_both, p)
     # remove both elements of p from lists of available symbols in col e[2]
     R[R$col == e[2], "avail"]$avail <- lapply(R[R$col == e[2], "avail"]$avail, remove_both, p)
+    
+    i <- i + 1
     
   }
   return(list(R, X))
