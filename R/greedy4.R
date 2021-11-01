46e6fbb27c9a0042e3b04262feddc8bca20bd972
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-11-01 14:54:16 +0000

    track available symbols as well

--- R/greedy4.R
+++ R/greedy4.R
@@ -23,24 +23,29 @@ greedy4 <- function(R, P = not_used_pairs(R), E = empty_cells(R)) {
     # remove cell p from the list of pairs
     P <- P[-match(list(p), P)]
     
-    # remove every pair sharing a common symbol with p from the same row as e
-    # remove every pair sharing a common symbol with p from the same column as e
     R <- R %>%
       mutate(
+        # these are the pairs in Pe containing p1
         Pe_p1 = map(Pe, pairs_containing, p[1]),
+        # these are hte pairs in Pe containing p2
         Pe_p2 = map(Pe, pairs_containing, p[2])
       ) %>%
       mutate(
+        # remove every pair containing p1 from cells in the same row or column as e
         Pe = case_when(
           row == e[1] | col == e[2] ~ map2(Pe, Pe_p1, remove_all_if_exist),
           TRUE ~ Pe
         )
       ) %>%
       mutate(
+        # remove every pair containing p2 from cells in the same row or column as e
         Pe = case_when(
           row == e[1] | col == e[2] ~ map2(Pe, Pe_p2, remove_all_if_exist),
           TRUE ~ Pe
         )
+      ) %>%
+      select(
+        -Pe_p1, -Pe_p2
       )
    
     # remove p from the list of available pairs for every remaining cell
@@ -48,6 +53,13 @@ greedy4 <- function(R, P = not_used_pairs(R), E = empty_cells(R)) {
       mutate(
         Pe = map(Pe, remove_if_exists, p)
       )
+    
+    # our implementation of is_maximal_proom currently depends upon the
+    # lists of available symbols being accurate
+    # remove both elements of p from lists of available symbols in row e[1]
+    R[R$row == e[1], "avail"]$avail <- lapply(R[R$row == e[1], "avail"]$avail, remove_both, p)
+    # remove both elements of p from lists of available symbols in col e[2]
+    R[R$col == e[2], "avail"]$avail <- lapply(R[R$col == e[2], "avail"]$avail, remove_both, p)
     
   }
   return(R)
