37c85e064737f68ff8b3116d1cab545a0bbdc7fa
Matthew Henderson <matthew.james.henderson@gmail.com>
2023-05-02 07:49:00 +0000

    remove functions not being used

--- R/log_to_df.R
+++
@@ -1,8 +0,0 @@
-log_to_df <- function(X, row_, col_, available_, Pe_, first_, second_, fill_, iter_, branch_) {
-  X <- X %>%
-    bind_rows(
-      tribble(  ~row,   ~col,      ~available, ~Pe, ~first, ~second, ~fill, ~iter, ~branch,
-                row_, col_, available_,  Pe_, first_, second_,  fill_, iter_, branch_)
-    )
-  return(X)
-}
