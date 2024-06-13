c55704d72d08dbd4d5aae964f235ed7bc9b559b2
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-06-13 07:49:56 +0000

    Use native pipe and package prefixes.

--- R/empty_room.R
+++ R/empty_room.R
@@ -1,4 +1,4 @@
 empty_room <- function(n = 5) {
-  expand_grid(row = 1:(n - 1), col = 1:(n - 1)) %>%
-    mutate(first = as.numeric(NA), second = as.numeric(NA))
+  tidyr::expand_grid(row = 1:(n - 1), col = 1:(n - 1)) |>
+    dplyr::mutate(first = as.numeric(NA), second = as.numeric(NA))
 }
--- R/plot_room_square.R
+++ R/plot_room_square.R
@@ -3,10 +3,10 @@ plot_room_square <- function(R) {
   n <- max(R$col) + 1
   
   ggplot2::ggplot(data = R, ggplot2::aes(col, row)) +
-    ggplot2::geom_tile(data = R %>% dplyr::filter(!is.na(first)), ggplot2::aes(fill = visit)) +
-    ggplot2::geom_segment(data = grid_lines(n - 1, n - 1), ggplot2::aes(x = x, y = y, xend = xend, yend = yend), size = .1) +
-    ggplot2::geom_text(data = R %>% dplyr::filter(!is.na(first)), ggplot2::aes(label = paste(first, second, sep = ","))) +
-    ggplot2::geom_text(data = R %>% dplyr::filter(!is.na(first)), ggplot2::aes(label = paste(first, second, sep = ","))) +
+    ggplot2::geom_tile(data = R |> dplyr::filter(!is.na(first)), ggplot2::aes(fill = visit)) +
+    ggplot2::geom_segment(data = grid_lines(n - 1, n - 1), ggplot2::aes(x = x, y = y, xend = xend, yend = yend), linewidth = .1) +
+    ggplot2::geom_text(data = R |> dplyr::filter(!is.na(first)), ggplot2::aes(label = paste(first, second, sep = ","))) +
+    ggplot2::geom_text(data = R |> dplyr::filter(!is.na(first)), ggplot2::aes(label = paste(first, second, sep = ","))) +
     ggplot2::scale_y_reverse() +
     ggplot2::coord_fixed() + 
     ggplot2::theme_void() +
@@ -21,6 +21,7 @@ plot_room_square_labs <- function(R) {
   n <- max(R$col) + 1
   
   nf <- n_filled_cells(R)
+  
   v <- volume(R)
   
   plot_room_square(R) +
