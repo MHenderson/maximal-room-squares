6b45e0c60e60f587aa77fed3758ea79c710425c1
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-06-18 08:16:42 +0100

    Use wallis plotting functions (#13)
    * Remove old plotting functions.
    
    * Use plotting functions from wallis.

--- R/greedy1-example-plot.R
+++ R/greedy1-example-plot.R
@@ -1,17 +1,28 @@
+library(dplyr)
 library(ggplot2)
+library(glue)
 library(here)
+library(wallis)
 
 source(here("R", "empty_cells.R"))
 source(here("R", "empty_room.R"))
 source(here("R", "greedy1.R"))
-source(here("R", "grid_lines.R"))
 source(here("R", "n-filled-cells.R"))
 source(here("R", "not_used_pairs.R"))
-source(here("R", "plot_room_square.R"))
 source(here("R", "volume.R"))
 
-R <- greedy1(6)
+n <- 6
 
-plot_room_square_labs(R)
+R <- greedy1(n) |>
+  mutate(fill = visit)
+
+nf <- n_filled_cells(R)
+v <- volume(R)
+
+plot_room_square(R) +
+  labs(
+    caption = glue("Order: {n}\nFilled cells: {nf}\nVolume: {v}")
+  ) +
+  theme(plot.caption = element_text(hjust = 0)) 
 
 ggsave(here("plots", "greedy1-example-plot.png"), width = 3, height = 3, bg = "white")
