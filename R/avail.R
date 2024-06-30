40d7b1fe4f2a58a5763938d473cb0e0b38b6c066
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-06-30 13:49:46 +0100

    Remove remaining verification functions (#26)
    * Install wallis v0.0.0.9005 and remove igraph package.
    
    * Remove all remaining function definitions.

--- R/avail.R
+++
@@ -1,4 +0,0 @@
-avail <- function(R, p, e) {
-  available <- R[R$row == e[1] & R$col == e[2], "avail"]$avail[[1]]
-  p[1] %in% available && p[2] %in% available
-}
--- R/pairs_containing.R
+++
@@ -1,3 +0,0 @@
-pairs_containing <- function(P, x) {
-  P[map_lgl(map(P, `%in%`, x), ~.[1] | .[2])]
-}
